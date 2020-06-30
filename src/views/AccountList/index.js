ea46f749c759ebc9ce9e3eff93f16e28001abb62
Luu Tuan Nguyen <miketuannguyen@gmail.com>
2020-06-30 16:01:27 +0700

    add account list page

--- src/views/AccountList/index.js
+++ src/views/AccountList/index.js
@@ -1,22 +1,62 @@
 import React from 'react'
-import {Button} from 'antd'
-import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
+import {Table} from 'antd'
 import styled from 'styled-components'
+import {api} from '../../services'
+import moment from 'moment'
 
-const Wrapper = styled.div`
-  height: 100vh;
+const Wrapper = styled.main`
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   flex-direction: column;
   align-items: center;
+  margin-left: 70px;
+  position: absolute;
+  z-index: 1000;
 `
 
 const AccountListPage = () => {
+  const token = useSelector(state => state.user.token)
+  const [data, setData] = React.useState([])
+
+  const columns = [
+    {
+      title: 'Account ID',
+      dataIndex: 'id',
+      key: 'id',
+      align: 'center'
+    },
+    {
+      title: 'Balance',
+      dataIndex: 'balance',
+      key: 'balance'
+    },
+    {
+      title: 'Type',
+      dataIndex: 'type',
+      key: 'type'
+    },
+    {
+      title: 'Created At',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+      render: (createdAt) => moment(createdAt).format('DD/MM/YYYY HH:mm:ss')
+    }
+  ]
+
+  React.useEffect(() => {
+    const getData = async () => {
+      const {data} = await api.get('user/account', undefined, token)
+      if (data) {
+        setData(data.payload)
+      }
+    }
+    getData()
+  }, [token])
+
   return (
     <Wrapper>
-      <Link to='/'>
-        <Button type='primary'>Account list page</Button>
-      </Link>
+      <Table dataSource={data} pagination={false} columns={columns} />
     </Wrapper>
   )
 }
